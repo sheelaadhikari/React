12c9e1b79b69a635ba0201201252bd2e7a209405
sheelaadhikari <adhikarisheela1000@gmail.com>
2022-09-12 17:02:53 +0545

    first commit

---
+++ src/components/AddContact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddContact = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2> Contact Manager</h2>
+            </div>
+        </div>
+    );
+}
+
+export default AddContact; 
---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AddContact from './AddContact';
+import './App.css';
+import Header from './Header';
+
+
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <AddContact></AddContact>
+      {/* < ContactList></ContactList> */}
+    </div>
+  );
+}
+
+export default App;
