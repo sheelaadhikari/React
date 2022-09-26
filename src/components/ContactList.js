d5a8b7cfb2b90d84499643ef5e346470bdb24cb5
sheelaadhikari <adhikarisheela1000@gmail.com>
2022-09-26 21:53:27 +0545

    props

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,15 +1,26 @@
 import React from "react";
 const ContactList = (props) => {
-    console.log(props); 
-  
-
-
-
-
-
-    return(
-        <div className="ui celled list">Contact list</div>
+    console.log(props);
+    const renderContactList = props.contacts.map((contact)=> {
+    return (
+        <div className="item">
+            <div className="content">
+                <div className="header">{contact.name}</div>
+                <div> {contact.email}</div>
+            </div>
+            <i className="trash alternate outline icon"></i>
+        </div>
     );
+})
+
+
+
+
+
+
+return (
+    <div className="ui celled list"> {renderContactList}</div>
+);
     
 
 }
