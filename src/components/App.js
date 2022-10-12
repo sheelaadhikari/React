cd18e8d83500dcc55b566649ab1ebf6f4879986a
sheelaadhikari <adhikarisheela1000@gmail.com>
2022-10-12 10:32:41 +0545

    counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import AddContact from './AddContact';
 import './App.css';
 import Header from './Header';
 import ContactList from './ContactList';
 
- 
+
 
 
 function App() {
-const[contacts, setContacts]=useState([]);
+  const [contacts, setContacts] = useState([]);
+
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(counter + 1)
+  }
 
   return (
-    <div classN ame='container'>
+    <div className='container'>
+
+
       <Header></Header>
       <AddContact></AddContact>
       < ContactList contacts={contacts}></ContactList>
+      <h1>{counter}</h1>
+      <button onClick={increase} >increase</button>
     </div>
   );
 }
