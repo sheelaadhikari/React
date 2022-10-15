814f5b1c5222add301213e01ddf1662f0c50fccc
sheelaadhikari <adhikarisheela1000@gmail.com>
2022-10-15 09:53:33 +0545

    react hook

--- src/components/App.js
+++ src/components/App.js
@@ -8,14 +8,22 @@ import ContactList from './ContactList';
 
 
 function App() {
+  const LOCAL_STORAGE_KEY="contacts";
   const [contacts, setContacts] = useState([]);
-const addContactHandler=( contact) =>{
-  console.log(contact);
-setContacts([...contacts, contact]);
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
 
-};
+  };
+  useEffect(() => {
+   const retriveContacts= JSON.parse (localStorage.getItem(LOCAL_STORAGE_KEY));
+   if( retriveContacts)setContacts(retriveContacts);
+  }, [contacts]);
 
 
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
+   }, [contacts]);
 
 
   return (
@@ -25,8 +33,8 @@ setContacts([...contacts, contact]);
       <Header></Header>
       <AddContact addContactHandler={addContactHandler}></AddContact>
       < ContactList contacts={contacts}></ContactList>
-    
-      
+
+
     </div>
   );
 }
