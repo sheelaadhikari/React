f158a752617af607f6abe41fe2d10915c1b13e2f
Sheela Adhikari <71367240+sheelaadhikari@users.noreply.github.com>
2023-04-28 17:32:12 +0545

    Add files via upload

---
+++ user.js
@@ -0,0 +1,11 @@
+import React from "react";
+//functional component
+function User() {
+    return (
+        <div className="container">
+            <h2>User functional component</h2>
+        </div>
+    )
+}
+
+export default User;
